08d4ed1170b555f131aa00efabb0118c3a111ee1
sunitha436 <abdassunitha2@gmail.com>
2025-08-05 10:53:16 +0530

    Add files via upload

---
+++ toggled case.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char str[200];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';  // remove newline
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        if (islower(str[i]))
+            str[i] = toupper(str[i]);
+        else if (isupper(str[i]))
+            str[i] = tolower(str[i]);
+    }
+
+    printf("Toggled case string: %s\n", str);
+
+    return 0;
+}
