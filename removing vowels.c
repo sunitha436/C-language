912aef817a267c0d5dd7e389f0b7c48fe039ed17
sunitha436 <abdassunitha2@gmail.com>
2025-08-05 10:44:42 +0530

    Add files via upload

---
+++ removing vowels.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+//remove the vowels
+int main() {
+    char str[200], result[200];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    for(i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+        if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+            result[j++] = str[i];
+        }
+    }
+
+    result[j] = '\0';
+
+    printf("String after removing vowels: %s\n", result);
+
+    return 0;
+}
