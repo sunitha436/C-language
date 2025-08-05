9d867d3dcdb2a6a243ea7d3cadb7c5c74abf66c6
sunitha436 <abdassunitha2@gmail.com>
2025-08-05 10:49:21 +0530

    Add files via upload

---
+++ repalce vowels.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    for(i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            str[i] = '*';  // replace vowel with '*'
+        }
+    }
+
+    printf("String after replacing vowels: %s\n", str);
+
+    return 0;
+}
